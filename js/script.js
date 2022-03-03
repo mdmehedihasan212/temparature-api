a75303c2e0db61cc558c233c30862eb612c4925c
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-03 22:33:36 +0600

    add name and temp

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const API_KEY = `154d5473322a8cb5eab3005d5ba39eb6`;
+const searchCity = () => {
+    const inputCity = document.getElementById("input-city").value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => searchName(data))
+}
+
+const cityName = (id, name) => {
+    document.getElementById(id).innerText = name;
+}
+const searchName = (value) => {
+    console.log(value);
+    cityName("city-name", value.name);
+    cityName("temparature", value.main.temp);
+    cityName("clouds", value.weather[0].main);
+
+}
+
